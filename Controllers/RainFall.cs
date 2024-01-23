6455c7222822936ae17dc45e201e081b3d670ce4
homs <homie.pajimula@gmail.com>
2024-01-23 09:34:15 +0800

    Add project files.

---
+++ Controllers/RainFall.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sorted_Coding_Test.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RainFall : ControllerBase
+    {
+        [HttpGet]
+        public string RainFallReading(string stationId)
+        {
+            var returnObj = "";
+
+            return returnObj;
+        }
+    }
+}
