f915025ff0760bb2ed649cbaeef6292f33326b7f
homs <homie.pajimula@gmail.com>
2024-01-23 10:24:40 +0800

    UPDATED API

--- Controllers/RainFall.cs
+++ Controllers/RainFall.cs
@@ -6,12 +6,34 @@ namespace Sorted_Coding_Test.Controllers
     [Route("[controller]")]
     public class RainFall : ControllerBase
     {
-        [HttpGet]
-        public string RainFallReading(string stationId)
+        private HttpClient client;
+        public RainFall()
         {
-            var returnObj = "";
+            client = new HttpClient();
+            client.BaseAddress = new Uri("http://environment.data.gov.uk/flood-monitoring/");
+        }
 
-            return returnObj;
+        [HttpGet]
+        public async Task<string> RainFallReading(string stationId)
+        {
+            try
+            {
+                var returnObj = "";
+
+                HttpResponseMessage response = await client.GetAsync(
+                    $"id/stations/{stationId}/readings");
+                
+                if (response.IsSuccessStatusCode)
+                {
+                    returnObj = await response.Content.ReadAsStringAsync();
+                }
+                return returnObj;
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
         }
     }
 }
